2369b8c794d9d9ef6151ea23ddac17c0ee607dd5
Chris Maier <cjmaier3@gmail.com>
2017-02-19 14:11:02 -0600

    added champ name and title to custom image cell

--- LoLInfo/LoLInfo/Custom/CustomImageCell.xaml.cs
+++ LoLInfo/LoLInfo/Custom/CustomImageCell.xaml.cs
@@ -9,11 +9,26 @@ namespace LoLInfo.Custom
     {
         public static readonly BindableProperty ImageSourceProperty =
         BindableProperty.Create("ImageSource", typeof(string), typeof(CustomImageCell), "");
-
         public string ImageSource
         {
             get { return (string)GetValue(ImageSourceProperty); }
             set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public static readonly BindableProperty NameProperty =
+        BindableProperty.Create("Name", typeof(string), typeof(CustomImageCell), "");
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public static readonly BindableProperty TitleProperty =
+        BindableProperty.Create("Title", typeof(string), typeof(CustomImageCell), "");
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
         }
 
         public CustomImageCell()
