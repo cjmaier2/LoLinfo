d662df574dea98b171f568ee0ad2312345a3feff
Chris Maier <cjmaier3@gmail.com>
2017-02-19 22:43:04 -0600

    added item DTOs

---
+++ LoLInfo/LolInfo.Services/Extensions/ItemExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using LolInfo.Models;
+using LolInfo.Services.ServiceModels;
+
+namespace LolInfo.Services.Extensions
+{
+    public static class ItemExtensions
+    {
+        public static List<Item> ToItems(this ItemListDto itemListDto)
+        {
+            var items = new List<Item>();
+            foreach (KeyValuePair<string, ItemDto> entry in itemListDto.Data)
+            {
+                var item = entry.Value;
+                items.Add(new Item()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    SearchName = Regex.Replace(item.Name.ToUpper(), @"[^\w\.@-]", ""), //strip special characters
+                    Gold = item.Gold.Total,
+                    SquareImageUrl = string.Format(ServiceConstants.ItemImageUrl, item.Image.Full)
+                });
+            }
+            return items.OrderBy(c => c.Name).ToList();
+        }
+    }
+}
