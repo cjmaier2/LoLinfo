cb7b5407be0a4b585db04facad7bd08e84896e14
Chris Maier <cjmaier3@gmail.com>
2017-02-26 22:09:27 -0600

    added custom half transparent background label

---
+++ LoLInfo/LoLInfo/Custom/OpacityLabel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LoLInfo.Custom
+{
+    public class OpacityLabel : Label
+    {
+        public OpacityLabel()
+        {
+            BackgroundColor = Color.FromRgba(0, 0, 0, .5);
+        }
+    }
+}
