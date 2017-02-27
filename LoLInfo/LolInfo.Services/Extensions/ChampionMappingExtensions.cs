758efee53b583476338da82551b48bb280e886d9
Chris Maier <cjmaier3@gmail.com>
2017-02-26 22:10:40 -0600

    replace skin name with champ name if it's the default one (no skin)

--- LoLInfo/LolInfo.Services/Extensions/ChampionMappingExtensions.cs
+++ LoLInfo/LolInfo.Services/Extensions/ChampionMappingExtensions.cs
@@ -22,20 +22,20 @@ namespace LolInfo.Services.Extensions
                     SearchName = Regex.Replace(champ.Name.ToUpper(), @"[^\w\.@-]", ""), //strip special characters
                     Title = champ.Title,
                     SquareImageUrl = string.Format(ServiceConstants.ChampionSquareImageUrl, champ.Image.Full),
-                    Skins = GetSkins(champ.Key, champ.Skins)
+                    Skins = GetSkins(champ.Key, champ.Name, champ.Skins)
                 });
             }
             return champions.OrderBy(c => c.Name).ToList();
         }
 
-        public static List<Skin> GetSkins(string champKey, List<SkinDto> skinDtos)
+        public static List<Skin> GetSkins(string champKey, string champName, List<SkinDto> skinDtos)
         {
             var skins = new List<Skin>();
             foreach (var skinDto in skinDtos)
             {
                 skins.Add(new Skin
                 {
-                    Name = skinDto.Name,
+                    Name = skinDto.Name == "default" ? champName : skinDto.Name,
                     LoadingImageUrl = $"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{champKey}_{skinDto.Num}.jpg",
                     SplashImageUrl = $"http://ddragon.leagueoflegends.com/cdn/img/champion/splash/{champKey}_{skinDto.Num}.jpg"
                 });
